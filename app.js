863f293dde4282eee46f128e9027ab5024f19597
Adriana Winkelman <adriana.winkelman@gmail.com>
2020-05-09 16:54:12 -0400

    "added heroku deploy files on script folder"

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const express = require('express')
 var io = require('socket.io')(http);
 var world = require('./js/server_world');
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-app.get('/js/client_world.js', function(req, res){
+app.get('/js/client_world.js', function (req, res) {
     res.sendFile(__dirname + '/js/client_world.js');
 });
 
 app.use(express.static(path.join(__dirname, 'images')))
 
 // Handle connection
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log('a user connected');
 
     var id = socket.id;
@@ -26,30 +26,30 @@ io.on('connection', function(socket){
 
     socket.broadcast.emit('addOtherPlayer', player);
 
-    socket.on('requestOldPlayers', function(){
-        for (var i = 0; i < world.players.length; i++){
+    socket.on('requestOldPlayers', function () {
+        for (var i = 0; i < world.players.length; i++) {
             if (world.players[i].playerId != id)
                 socket.emit('addOtherPlayer', world.players[i]);
         }
     });
-    socket.on('updatePosition', function(data){
+    socket.on('updatePosition', function (data) {
         var newData = world.updatePlayerData(data);
         socket.broadcast.emit('updatePosition', newData);
     });
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         console.log('user disconnected');
         io.emit('removeOtherPlayer', player);
-        world.removePlayer( player );
+        world.removePlayer(player);
     });
 
 });
 
 // Handle environment changes
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
- 
-http.listen(port, ip_address, function(){
-    console.log( "Listening on " + ip_address + ", server_port " + port );
+var port = process.env.PORT || 8080;
+var ip_address = process.env.IP || '0.0.0.0';
+
+http.listen(port, ip_address, function () {
+    console.log("Listening on " + ip_address + ", server_port " + port);
 });
 
 /*
