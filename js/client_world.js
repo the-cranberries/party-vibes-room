08c8f4e97090027c4cf4d6ec8def23ec1a37f53d
jli09 <56615820+jli09@users.noreply.github.com>
2020-05-10 13:45:19 -0400

    adjusted player controls

--- js/client_world.js
+++ js/client_world.js
@@ -1,6 +1,9 @@
-
-
-var container, scene, camera, renderer, raycaster, objects = [];
+var container,
+  scene,
+  camera,
+  renderer,
+  raycaster,
+  objects = [];
 var keyState = {};
 var sphere;
 
@@ -8,181 +11,172 @@ var player, playerId, moveSpeed, turnSpeed;
 
 var playerData;
 
-var otherPlayers = [], otherPlayersId = [];
+var otherPlayers = [],
+  otherPlayersId = [];
 
 var loadWorld = function () {
+  init();
+  animate();
 
-    init();
-    animate();
+  function init() {
+    //Setup------------------------------------------
+    container = document.getElementById('container');
 
-    function init() {
+    scene = new THREE.Scene();
+    scene.position.x = 1;
+    scene.position.y = -5;
+    scene.position.z = 4;
 
-        //Setup------------------------------------------
-        container = document.getElementById('container');
+    camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      1,
+      1000
+    );
+    camera.position.y = 7;
+    camera.position.z = 20;
+    camera.position.x = 0;
+    camera.lookAt(scene.position);
 
-        scene = new THREE.Scene();
-        scene.position.x = 1
-        scene.position.y = -5
-        scene.position.z = 4
+    const loader = new THREE.TextureLoader();
+    const bgTexture = loader.load('background.png');
+    scene.background = bgTexture;
 
-        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-        camera.position.y = 7
-        camera.position.z = 20;
-        camera.position.x = 0;
-        camera.lookAt(scene.position)
+    renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
+    raycaster = new THREE.Raycaster();
+    //Add Objects To the Scene HERE-------------------
 
+    //Sphere------------------
+    // var sphere_geometry = new THREE.SphereGeometry(1);
+    // var sphere_material = new THREE.MeshNormalMaterial();
+    // sphere = new THREE.Mesh(sphere_geometry, sphere_material);
 
-        const loader = new THREE.TextureLoader();
-        const bgTexture = loader.load('background.png');
-        scene.background = bgTexture;
+    // scene.add(sphere);
+    // objects.push(sphere); //if you are interested in detecting an intersection with this sphere
 
-        renderer = new THREE.WebGLRenderer({ alpha: true });
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
+    //Events------------------------------------------
+    document.addEventListener('click', onMouseClick, false);
+    document.addEventListener('mousedown', onMouseDown, false);
+    document.addEventListener('mouseup', onMouseUp, false);
+    document.addEventListener('mousemove', onMouseMove, false);
+    document.addEventListener('mouseout', onMouseOut, false);
+    document.addEventListener('keydown', onKeyDown, false);
+    document.addEventListener('keyup', onKeyUp, false);
+    window.addEventListener('resize', onWindowResize, false);
 
-        raycaster = new THREE.Raycaster();
-        //Add Objects To the Scene HERE-------------------
+    //Final touches-----------------------------------
+    container.appendChild(renderer.domElement);
+    document.body.appendChild(container);
+  }
 
-        //Sphere------------------
-        // var sphere_geometry = new THREE.SphereGeometry(1);
-        // var sphere_material = new THREE.MeshNormalMaterial();
-        // sphere = new THREE.Mesh(sphere_geometry, sphere_material);
+  function animate() {
+    requestAnimationFrame(animate);
+    render();
+  }
+  function render() {
+    if (player) {
+      // updateCameraPosition();
 
-        // scene.add(sphere);
-        // objects.push(sphere); //if you are interested in detecting an intersection with this sphere
+      checkKeyStates();
 
-        //Events------------------------------------------
-        document.addEventListener('click', onMouseClick, false);
-        document.addEventListener('mousedown', onMouseDown, false);
-        document.addEventListener('mouseup', onMouseUp, false);
-        document.addEventListener('mousemove', onMouseMove, false);
-        document.addEventListener('mouseout', onMouseOut, false);
-        document.addEventListener('keydown', onKeyDown, false);
-        document.addEventListener('keyup', onKeyUp, false);
-        window.addEventListener('resize', onWindowResize, false);
-
-        //Final touches-----------------------------------
-        container.appendChild(renderer.domElement);
-        document.body.appendChild(container);
+      // camera.lookAt( player.position );
     }
+    //Render Scene---------------------------------------
+    renderer.clear();
+    renderer.render(scene, camera);
+  }
 
-    function animate() {
-        requestAnimationFrame(animate);
-        render();
+  function onMouseClick() {
+    intersects = calculateIntersects(event);
+
+    if (intersects.length > 0) {
+      //If object is intersected by mouse pointer, do something
+      if (intersects[0].object == sphere) {
+        alert('This is a sphere!');
+      }
     }
-    function render() {
+  }
+  function onMouseDown() {}
+  function onMouseUp() {}
+  function onMouseMove() {}
+  function onMouseOut() {}
+  function onKeyDown(event) {
+    //event = event || window.event;
 
-        if (player) {
+    keyState[event.keyCode || event.which] = true;
+  }
 
-            // updateCameraPosition();
+  function onKeyUp(event) {
+    //event = event || window.event;
 
-            checkKeyStates();
+    keyState[event.keyCode || event.which] = false;
+  }
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-            // camera.lookAt( player.position );
-        }
-        //Render Scene---------------------------------------
-        renderer.clear();
-        renderer.render(scene, camera);
-    }
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+  function calculateIntersects(event) {
+    //Determine objects intersected by raycaster
+    event.preventDefault();
 
-    function onMouseClick() {
-        intersects = calculateIntersects(event);
+    var vector = new THREE.Vector3();
+    vector.set(
+      (event.clientX / window.innerWidth) * 2 - 1,
+      -(event.clientY / window.innerHeight) * 2 + 1,
+      0.5
+    );
+    vector.unproject(camera);
 
-        if (intersects.length > 0) {
-            //If object is intersected by mouse pointer, do something
-            if (intersects[0].object == sphere) {
-                alert("This is a sphere!");
-            }
-        }
-    }
-    function onMouseDown() {
+    raycaster.ray.set(camera.position, vector.sub(camera.position).normalize());
 
-    }
-    function onMouseUp() {
+    var intersects = raycaster.intersectObjects(objects);
 
-    }
-    function onMouseMove() {
-
-    }
-    function onMouseOut() {
-
-    }
-    function onKeyDown(event) {
-
-        //event = event || window.event;
-
-        keyState[event.keyCode || event.which] = true;
-
-    }
-
-    function onKeyUp(event) {
-
-        //event = event || window.event;
-
-        keyState[event.keyCode || event.which] = false;
-
-    }
-    function onWindowResize() {
-
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-
-        renderer.setSize(window.innerWidth, window.innerHeight);
-
-    }
-    function calculateIntersects(event) {
-
-        //Determine objects intersected by raycaster
-        event.preventDefault();
-
-        var vector = new THREE.Vector3();
-        vector.set((event.clientX / window.innerWidth) * 2 - 1, - (event.clientY / window.innerHeight) * 2 + 1, 0.5);
-        vector.unproject(camera);
-
-        raycaster.ray.set(camera.position, vector.sub(camera.position).normalize());
-
-        var intersects = raycaster.intersectObjects(objects);
-
-        return intersects;
-    }
-
+    return intersects;
+  }
 };
 
 var createPlayer = function (data) {
+  playerData = data;
 
-    playerData = data;
+  var cube_geometry = new THREE.BoxGeometry(
+    data.sizeX * 2,
+    data.sizeY * 4,
+    data.sizeZ * 2
+  );
+  const iconLoader = new THREE.TextureLoader();
+  const cube_material = [
+    new THREE.MeshBasicMaterial({ color: 0x69bdd2 }),
+    new THREE.MeshBasicMaterial({ color: 0x69bdd2 }),
+    new THREE.MeshBasicMaterial({ color: 0x407294 }),
+    new THREE.MeshBasicMaterial({ color: 0x69bdd2 }),
+    new THREE.MeshBasicMaterial({ map: iconLoader.load('pietro.jpg') }), //front of object avatar
 
+    new THREE.MeshBasicMaterial({ color: 0x407294 }),
+  ];
+  player = new THREE.Mesh(cube_geometry, cube_material);
 
-    var cube_geometry = new THREE.BoxGeometry(data.sizeX * 2, data.sizeY * 4, data.sizeZ * 2);
-    const iconLoader = new THREE.TextureLoader();
-    const cube_material = [
-        new THREE.MeshBasicMaterial({ color: 0x69bdd2 }),
-        new THREE.MeshBasicMaterial({ color: 0x69bdd2 }),
-        new THREE.MeshBasicMaterial({ color: 0x407294 }),
-        new THREE.MeshBasicMaterial({ color: 0x69bdd2 }),
-        new THREE.MeshBasicMaterial({ color: 0x407294 }),
-        new THREE.MeshBasicMaterial({ map: iconLoader.load('pietro.jpg') }),//front of object avatar
-    ]
-    player = new THREE.Mesh(cube_geometry, cube_material);
+  player.rotation.set(0, 0, 0);
 
-    player.rotation.set(0, 0, 0);
+  player.position.x = data.x;
+  player.position.y = data.y;
+  player.position.z = data.z;
 
-    player.position.x = data.x;
-    player.position.y = data.y;
-    player.position.z = data.z;
+  playerId = data.playerId;
+  moveSpeed = data.speed;
+  turnSpeed = data.turnSpeed;
 
-    playerId = data.playerId;
-    moveSpeed = data.speed;
-    turnSpeed = data.turnSpeed;
+  // updateCameraPosition();
 
-    // updateCameraPosition();
+  objects.push(player);
+  scene.add(player);
 
-    objects.push(player);
-    scene.add(player);
-
-    camera.lookAt(player.position);
+  camera.lookAt(player.position);
 };
 
 // var updateCameraPosition = function(){
@@ -194,111 +188,107 @@ var createPlayer = function (data) {
 // };
 
 var updatePlayerPosition = function (data) {
+  var somePlayer = playerForId(data.playerId);
 
-    var somePlayer = playerForId(data.playerId);
+  somePlayer.position.x = data.x;
+  somePlayer.position.y = data.y;
+  somePlayer.position.z = data.z;
 
-    somePlayer.position.x = data.x;
-    somePlayer.position.y = data.y;
-    somePlayer.position.z = data.z;
-
-    somePlayer.rotation.x = data.r_x;
-    somePlayer.rotation.y = data.r_y;
-    somePlayer.rotation.z = data.r_z;
-
+  somePlayer.rotation.x = data.r_x;
+  somePlayer.rotation.y = data.r_y;
+  somePlayer.rotation.z = data.r_z;
 };
 
 var updatePlayerData = function () {
-    playerData.x = player.position.x;
-    playerData.y = player.position.y;
-    playerData.z = player.position.z;
+  playerData.x = player.position.x;
+  playerData.y = player.position.y;
+  playerData.z = player.position.z;
 
-    playerData.r_x = player.rotation.x;
-    playerData.r_y = player.rotation.y;
-    playerData.r_z = player.rotation.z;
-
+  playerData.r_x = player.rotation.x;
+  playerData.r_y = player.rotation.y;
+  playerData.r_z = player.rotation.z;
 };
 var checkKeyStates = function () {
-
-    if (keyState[38] || keyState[87]) {
-        // up arrow or 'w' - move forward
-        player.position.x -= moveSpeed * Math.sin(player.rotation.y);
-        player.position.z -= moveSpeed * Math.cos(player.rotation.y);
-        updatePlayerData();
-        socket.emit('updatePosition', playerData);
-    }
-    if (keyState[40] || keyState[83]) {
-        // down arrow or 's' - move backward
-        player.position.x += moveSpeed * Math.sin(player.rotation.y);
-        player.position.z += moveSpeed * Math.cos(player.rotation.y);
-        updatePlayerData();
-        socket.emit('updatePosition', playerData);
-    }
-    if (keyState[37] || keyState[65]) {
-        // left arrow or 'a' - rotate left
-        player.rotation.y += turnSpeed;
-        updatePlayerData();
-        socket.emit('updatePosition', playerData);
-    }
-    if (keyState[39] || keyState[68]) {
-        // right arrow or 'd' - rotate right
-        player.rotation.y -= turnSpeed;
-        updatePlayerData();
-        socket.emit('updatePosition', playerData);
-    }
-    if (keyState[81]) {
-        // 'q' - strafe left
-        player.position.x -= moveSpeed * Math.cos(player.rotation.y);
-        player.position.z += moveSpeed * Math.sin(player.rotation.y);
-        updatePlayerData();
-        socket.emit('updatePosition', playerData);
-    }
-    if (keyState[69]) {
-        // 'e' - strage right
-        player.position.x += moveSpeed * Math.cos(player.rotation.y);
-        player.position.z -= moveSpeed * Math.sin(player.rotation.y);
-        updatePlayerData();
-        socket.emit('updatePosition', playerData);
-    }
-
+  if (keyState[38] || keyState[87]) {
+    //up arrow or 'w' - move forward
+    player.position.x -= moveSpeed * Math.sin(player.rotation.y);
+    player.position.z -= moveSpeed * Math.cos(player.rotation.y);
+    updatePlayerData();
+    socket.emit('updatePosition', playerData);
+  }
+  if (keyState[40] || keyState[83]) {
+    //down arrow or 's' - move backward
+    player.position.x += moveSpeed * Math.sin(player.rotation.y);
+    player.position.z += moveSpeed * Math.cos(player.rotation.y);
+    updatePlayerData();
+    socket.emit('updatePosition', playerData);
+  }
+  if (keyState[37]) {
+    // 'left arrow' - move left
+    player.position.x -= moveSpeed * Math.cos(player.rotation.y);
+    player.position.z += moveSpeed * Math.sin(player.rotation.y);
+    updatePlayerData();
+    socket.emit('updatePosition', playerData);
+  }
+  if (keyState[39]) {
+    // 'right arrow' - move right
+    player.position.x += moveSpeed * Math.cos(player.rotation.y);
+    player.position.z -= moveSpeed * Math.sin(player.rotation.y);
+    updatePlayerData();
+    socket.emit('updatePosition', playerData);
+  }
+  if (keyState[65]) {
+    // a  - rotate left
+    player.rotation.y += turnSpeed;
+    updatePlayerData();
+    socket.emit('updatePosition', playerData);
+  }
+  if (keyState[68]) {
+    // d  - rotate right
+    player.rotation.y -= turnSpeed;
+    updatePlayerData();
+    socket.emit('updatePosition', playerData);
+  }
 };
 
 var addOtherPlayer = function (data) {
-    var cube_geometry = new THREE.BoxGeometry(data.sizeX * 2, data.sizeY * 4, data.sizeZ * 2);
-    const iconLoader = new THREE.TextureLoader();
-    const cube_material = [
-        new THREE.MeshBasicMaterial({ color: 0x69bdd2 }),
-        new THREE.MeshBasicMaterial({ color: 0x69bdd2 }),
-        new THREE.MeshBasicMaterial({ color: 0x407294 }),
-        new THREE.MeshBasicMaterial({ color: 0x69bdd2 }),
-        new THREE.MeshBasicMaterial({ color: 0x407294 }),
-        new THREE.MeshBasicMaterial({ map: iconLoader.load('pietro.jpg') }),//front of object avatar
-    ]
-    var otherPlayer = new THREE.Mesh(cube_geometry, cube_material);
+  var cube_geometry = new THREE.BoxGeometry(
+    data.sizeX * 2,
+    data.sizeY * 4,
+    data.sizeZ * 2
+  );
+  const iconLoader = new THREE.TextureLoader();
+  const cube_material = [
+    new THREE.MeshBasicMaterial({ color: 0x69bdd2 }),
+    new THREE.MeshBasicMaterial({ color: 0x69bdd2 }),
+    new THREE.MeshBasicMaterial({ color: 0x407294 }),
+    new THREE.MeshBasicMaterial({ color: 0x69bdd2 }),
+    new THREE.MeshBasicMaterial({ color: 0x407294 }),
+    new THREE.MeshBasicMaterial({ map: iconLoader.load('pietro.jpg') }), //front of object avatar
+  ];
+  var otherPlayer = new THREE.Mesh(cube_geometry, cube_material);
 
-    otherPlayer.position.x = data.x;
-    otherPlayer.position.y = data.y;
-    otherPlayer.position.z = data.z;
+  otherPlayer.position.x = data.x;
+  otherPlayer.position.y = data.y;
+  otherPlayer.position.z = data.z;
 
-    otherPlayersId.push(data.playerId);
-    otherPlayers.push(otherPlayer);
-    objects.push(otherPlayer);
-    scene.add(otherPlayer);
-
+  otherPlayersId.push(data.playerId);
+  otherPlayers.push(otherPlayer);
+  objects.push(otherPlayer);
+  scene.add(otherPlayer);
 };
 
 var removeOtherPlayer = function (data) {
-
-    scene.remove(playerForId(data.playerId));
-
+  scene.remove(playerForId(data.playerId));
 };
 
 var playerForId = function (id) {
-    var index;
-    for (var i = 0; i < otherPlayersId.length; i++) {
-        if (otherPlayersId[i] == id) {
-            index = i;
-            break;
-        }
+  var index;
+  for (var i = 0; i < otherPlayersId.length; i++) {
+    if (otherPlayersId[i] == id) {
+      index = i;
+      break;
     }
-    return otherPlayers[index];
+  }
+  return otherPlayers[index];
 };
